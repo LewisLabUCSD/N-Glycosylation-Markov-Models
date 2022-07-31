58d4a0822833f04c11b0bfd4f603ac821cac21a4
guangchenlia <41449937+guangchenlia@users.noreply.github.com>
2022-07-31 09:48:42 -0700

    Updated Jul-31-2022

--- AUX Functions/GetGlycanCompositions.m
+++ AUX Functions/GetGlycanCompositions.m
@@ -4,14 +4,14 @@ compositions = cell(size(linearcodes));
 
 for k1 = 1:length(linearcodes)
     str = linearcodes{k1};
-    composition_monoCounts_pre(k1,1) = length(strfind(str,'F'));
-    composition_monoCounts_pre(k1,2) = length(strfind(str,'NN'));
+    composition_monoCounts_pre(k1,1) = length(strfind(str,'NN'));
+    composition_monoCounts_pre(k1,2) = length(strfind(str,'F'));
     composition_monoCounts_pre(k1,3) = length(strfind(str,'A')) + length(strfind(str,'M'))...
         -length(strfind(str,'Asn'))-length(strfind(str,'AN'));% discount for Asn and AN
     composition_monoCounts_pre(k1,4) = length(strfind(str,'GN'));
 end
 
-generaltypes = {'dHex','NeuAc','Hex','HexNAc'};
+generaltypes = {'NeuAc','dHex','Hex','HexNAc'};
 
 for a = 1:length(compositions)
     str = [];
